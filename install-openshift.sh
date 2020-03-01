7a5016019168fe95ece62cf51bbc72d8021caa3f
Damien Clément d'Huart <damien.clementdhuart@gmail.com>
2020-03-01 23:54:17 +0100

    update

--- install-openshift.sh
+++ install-openshift.sh
@@ -116,7 +116,7 @@ yum -y --enablerepo=epel downgrade install ansible.rpm
 cat <<EOD > /etc/hosts
 127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4 
 ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
-${IP}		$(hostname) console console.${DOMAIN}  
+${IP}		$(hostname) console console.${DOMAIN} master.localhost.localdomain
 EOD
 
 if [ -z $DISK ]; then 
